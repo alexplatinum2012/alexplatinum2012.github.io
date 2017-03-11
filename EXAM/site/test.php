0a1ea6d9fe1629f9dc05e918efd0de0fe4df63eb
Pestrak Aleksandr <alexplatinum2012@gmail.com>
2017-03-11 17:07:38 +0500

    exam

---
+++ EXAM/site/test.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>123</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <!--motobyker-->
+    <div class="img-wrapper">
+      <img src="img/home_header.jpg" alt="motobyker">
+    </div>
+    <!--end motobyker-->
+
+    <!--Wrapper-->
+    <div class="wrapper">
+      <header class="header">
+        <div class="header-logo">
+          HEADER LOGO
+        </div>
+        <div class="header-menu">
+          <ul class="inline-list">
+            <li class="inline-list-item"><a href="#">КАТЕГОРИЯ 1</a></li>
+            <li class="inline-list-item"><a href="#">КАТЕГОРИЯ 2</a></li>
+            <li class="inline-list-item"><a href="#">КАТЕГОРИЯ 3</a></li>
+            <li class="inline-list-item"><a href="#">КАТЕГОРИЯ 4</a></li>
+          </ul>
+        </div>
+        <div class="header-right">
+          <div class="right-user">
+            <div class="user-icon">
+              <img src="img/header_user_icon.png" alt="">
+            </div>
+            <ul class="inline-list">
+              <li><a href="#">Войти</a></li>
+              <li><a href="#">Регистрация</a></li>
+            </ul>
+          </div>
+          <div class="right-cart">
+            cart
+          </div>
+
+        </div>
+      </header>
+    </div>
+
+    <!--End wrapper-->
+  </body>
+</html>
